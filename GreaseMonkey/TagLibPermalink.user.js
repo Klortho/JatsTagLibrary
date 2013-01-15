eeda977011d8da0175575c3f8cb627a05ea6665f
Chris Maloney <voldrani@gmail.com>
2013-01-15 17:23:42 -0500

    Make domains a little more generic

--- GreaseMonkey/TagLibPermalink.user.js
+++ GreaseMonkey/TagLibPermalink.user.js
@@ -5,14 +5,8 @@
 // @namespace     TagLibPermalink
 // @version       1.0.1
 // @description   Greasemonkey script to add a permalink to the tag library documentation pages.
-// @include       http://jats.nlm.nih.gov/archiving/tag-library/*
-// @include       http://jats.nlm.nih.gov/articleauthoring/tag-library/*
-// @include       http://jats.nlm.nih.gov/extensions/bits/tag-library/*
-// @include       http://jats.nlm.nih.gov/publishing/tag-library/*
-// @include       http://dtd.nlm.nih.gov/book/tag-library/*
-// @include       http://dtd.nlm.nih.gov/archiving/tag-library/*
-// @include       http://dtd.nlm.nih.gov/articleauthoring/tag-library/*
-// @include       http://dtd.nlm.nih.gov/publishing/tag-library/*
+// @include       http://jats.nlm.nih.gov/*/tag-library/*
+// @include       http://dtd.nlm.nih.gov/*/tag-library/*
 // ==/UserScript==
 
 
