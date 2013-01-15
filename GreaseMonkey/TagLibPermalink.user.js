239ca0ceca0facf2078319e6ec78bfaa68805996
Chris Maloney <voldrani@gmail.com>
2013-01-14 21:12:14 -0500

    First cut of TagLibPermalink script done; works on all jats and dtd tag libraries

--- GreaseMonkey/TagLibPermalink.user.js
+++ GreaseMonkey/TagLibPermalink.user.js
@@ -1,58 +1,74 @@
-// Greasemonkey script to add a permalink to the tag library.
-// HT http://joanpiedra.com/jquery/greasemonkey/.
+// Greasemonkey script to add a permalink to the tag library documentation pages.
 //
 // ==UserScript==
-// @name          tspl
-// @namespace     tspl
-// @description   Greasemonkey script to add a permalink to the tag library.
-// @include       http://jats.nlm.nih.gov/*
+// @name          TagLibPermalink
+// @namespace     TagLibPermalink
+// @description   Greasemonkey script to add a permalink to the tag library documentation pages.
+// @include       http://jats.nlm.nih.gov/archiving/tag-library/*
+// @include       http://jats.nlm.nih.gov/articleauthoring/tag-library/*
+// @include       http://jats.nlm.nih.gov/publishing/tag-library/*
+// @include       http://dtd.nlm.nih.gov/book/tag-library/*
+// @include       http://dtd.nlm.nih.gov/archiving/tag-library/*
+// @include       http://dtd.nlm.nih.gov/articleauthoring/tag-library/*
+// @include       http://dtd.nlm.nih.gov/publishing/tag-library/*
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // ==/UserScript==
 
-
+// Prevent it executing when inside one of the frame documents
 if (window.top == window.self) {
 
-    //setTimeout(function() {
-    $('frame[name="main"]').load( function() {
-        var mainframe = top.frames["main"].document;
-        console.info("mainframe is " + mainframe);
-        var pageheader = $('body.main div.pageheader', mainframe);
-        var next = pageheader.next();
-        var header = next.is('h1') ? next : next.children('h1').filter(':first');
-        //var header = $(pageheader.next('h1, div h1');
+    // Store the original, default value for the src attribute of the main frame.
+    // This will be the filename of the title page.  E.g. "n-c000.html".
+    var mainFrame = $('frame[name="main"]');
+    var titleFile = mainFrame.attr('src');
 
-        // Get the main frame's key:
-        // (returns http://jats.nlm.nih.gov/archiving/tag-library/1.0/n-fm00.html)
-        var mainUrl = mainframe.location.toString();
-        var mainFilename = mainUrl.match("[-a-zA-Z0-9]+\.html")[0];
-        var type, slug;
+    mainFrame.load( function() {
+        var mainDoc = top.frames["main"].document;
+
+        // Get the main frame's filename (e.g. "n-fm00.html")
+        var fnm = mainDoc.location.toString().match("[-a-zA-Z0-9]+\.html");
+        if (!fnm) return;
+        var mainFilename = fnm[0];
+
+        // If the browser is pointed at a URL like "...?elem=attrib", then the mainFrame will
+        // load twice:  once with the page that is hard-coded in the src attribute (the title
+        // page, e.g. "n-c000.html") and once with the page that is found by the Mulberry
+        // JavaScript.  We are only interested in the latter.
+        if (mainFilename == titleFile) return;
+
+        var hw = $('body.main div.pageheader', mainDoc).next(); // header wrapper
+        var header = hw.is('h1') ? hw : hw.children('h1, h3').filter(':first');
+
         var types = {
             "elem": elems,
             "attr": attrs,
             "pe": pes,
             "chap": chaps
         };
-        for (type in types) {
+        for (var type in types) {
             var xref = types[type];
             for (var name in xref) {
-                //console.info("type " + type + ", name: " + name);
                 if (xref[name] == mainFilename) {
-                    slug = name;
+                    var slug = name;
                     break;
                 }
             }
             if (typeof(slug) != "undefined") break;
         }
-        console.info("type " + type + ", slug " + slug);
 
-        var realLoc = window.location.toString();
-        var loc = realLoc.replace(/\?.*/, "");
-        var permahref = loc + "?" + type + "=" + slug;
-        console.info("permahref = " + permahref);
-        var permalink = $('<a href="' + permahref + '" target="_top">permalink</a>');
+        var permalink;
+        if (typeof(slug) == "undefined") {
+            // Couldn't find this page.  This happens for several of the "chap"
+            // pages.
+            permalink = $('<em>[ no permalink ]</em>');
+        }
+        else {
+            var loc = window.location.toString().replace(/\?.*/, "");   // strip off query string and fragment
+            var permahref = loc + "?" + type + "=" + slug;
+            permalink = $('<a href="' + permahref + '" target="_top">permalink</a>');
+        }
         permalink.css({ "float": "right" });
         header.before(permalink);
-    //}, 2000);
     });
 }
 
