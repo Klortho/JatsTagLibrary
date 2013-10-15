52ec3162ba4ba630884807d47cecc9346a9db7a3
Chris Maloney <voldrani@gmail.com>
2013-10-15 06:21:41 -0400

    Shuffle content around, fix make-docs.rb script accordingly

--- scripts/make-docs.rb
+++ scripts/make-docs.rb
@@ -3,55 +3,87 @@
 # required by the new library browser framework.
 # It depends on saxon9he.jar being in the same directory as this script.
 
+require 'fileutils'
 require 'nokogiri'
 require 'trollop'
 
 opts = Trollop::options do
   banner <<-EOB
-Convert JATS Tag Library documentation to format required by jqapi library.
+Convert JATS Tag Library documentation to format required by jatsdoc library.
 
 Usage: #{__FILE__} [options] [file-to-convert]
 
+By default, this transforms all the XHTML files specified by the toc-xref.xml
+file, and puts the results into jatsdoc/entries.  It also copies all of image
+files from graphics into jatsdoc/resources.
+
 [file-to-convert], if given, specifies the file either by the hash, slug, or
 the title (see toc-xref.xml).  If not given, then all the files are converted.
+
+Options:
 EOB
+  opt :copy_resources, "Only copy resources"
 end
 
 Trollop::die "Too many arguments" if ARGV.size > 1
 file_to_convert = ARGV[0]  # This will be nil if not given
 
+#p opts
+#p opts[:copy_resources]
+#exit
+
 script_dir = File.dirname(__FILE__)
-entries_dir = "jqapi-docs/entries"
-Dir.mkdir(entries_dir) unless File.directory?(entries_dir)
 
-converted = false
-doc = Nokogiri::XML(File.open("toc-xref.xml"))
-doc.xpath('//item').each { |item|
-  hash = item.attribute("hash").to_str
-  title = item.attribute("title").to_str
-  slug = item.attribute("slug").to_str
+dest_dir = "jatsdoc"
 
-  if hash != "" && (!file_to_convert || file_to_convert == hash ||
-      file_to_convert == title || file_to_convert == slug)
+entries_src_dir = "orig-html"
+entries_dest_dir = "#{dest_dir}/entries"
 
-    in_html = "#{hash}.html"
-    out_html = "#{entries_dir}/#{slug}.html"
-    puts "Transforming #{in_html} -> #{out_html}"
-    transform_cmd =
-      "java -jar #{script_dir}/saxon9he.jar " +
-      "-xsl:#{script_dir}/make-doc.xsl " +
-      "-s:#{in_html} " +
-      "> #{out_html}"
+resources_src_dir = "graphics"
+resources_dest_dir = "#{dest_dir}/resources"
 
-    %x( #{transform_cmd} )
-    if $?.exitstatus != 0
-      puts "Failed: '#{transform_cmd}', exit status is #{$?.exitstatus}"
-      exit
+Dir.mkdir(dest_dir) unless File.directory?(dest_dir)
+Dir.mkdir(entries_dest_dir) unless File.directory?(entries_dest_dir)
+
+if !opts[:copy_resources]
+  converted = false
+  doc = Nokogiri::XML(File.open("toc-xref.xml"))
+  doc.xpath('//item').each { |item|
+    hash = item.attribute("hash").to_str
+    title = item.attribute("title").to_str
+    slug = item.attribute("slug").to_str
+
+    if hash != "" && (!file_to_convert || file_to_convert == hash ||
+        file_to_convert == title || file_to_convert == slug)
+
+      in_html = "#{entries_src_dir}/#{hash}.html"
+      out_html = "#{entries_dest_dir}/#{slug}.html"
+      puts "Transforming #{in_html} -> #{out_html}"
+      transform_cmd =
+        "java -jar #{script_dir}/saxon9he.jar " +
+        "-xsl:#{script_dir}/make-doc.xsl " +
+        "-s:#{in_html} " +
+        "> #{out_html}"
+
+      %x( #{transform_cmd} )
+      if $?.exitstatus != 0
+        puts "Failed: '#{transform_cmd}', exit status is #{$?.exitstatus}"
+        exit
+      end
+      converted = true
     end
-    converted = true
-  end
-}
+  }
 
-if !converted
-  puts "No matching file found"
+  if !converted
+    puts "No matching file found"
+  end
+end
+
+# Copy the resources, if there was no "file_to_convert" argument given
+if !file_to_convert || opts[:copy_resources]
+  FileUtils.rm_rf resources_dest_dir
+  #Dir.mkdir(resources_dest_dir) unless File.directory?(resources_dest_dir)
+
+
+  FileUtils.cp_r("#{resources_src_dir}", resources_dest_dir)
 end
